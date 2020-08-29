783e29382f2e9f44802f2d054b179a4b4b9af229
Taher Lokhandwala <taher.mar1999@gmail.com>
2020-08-29 18:53:20 +0530

    Assignment1.py all functions implemented

--- assignment1/Assignment1.py
+++ assignment1/Assignment1.py
@@ -10,8 +10,24 @@ import random
 # output:int/float/double/large
 
 
+def calculate_entropy(p, n):
+    entropy = 0
+    if p > 0 and p+n > 0:
+        entropy += (-p/(p+n))*np.log2(p/(p+n))
+    if n > 0 and p+n > 0:
+        entropy += (-n/(p+n))*np.log2(n/(p+n))
+    return entropy
+
+
 def get_entropy_of_dataset(df):
     entropy = 0
+    p = n = 0
+    for i in df.iloc[:, -1]:
+        if i == "yes":
+            p += 1
+        elif i == "no":
+            n += 1
+    entropy = calculate_entropy(p, n)
     return entropy
 
 
@@ -22,6 +38,12 @@ def get_entropy_of_dataset(df):
 
 def get_entropy_of_attribute(df, attribute):
     entropy_of_attribute = 0
+    unique_values = df[attribute].unique()
+    for i in unique_values:
+        sub_df = df[df[attribute] == i]
+        entropy = get_entropy_of_dataset(sub_df)
+        entropy_of_attribute += (sub_df.shape[0]/df.shape[0]) * entropy
+
     return abs(entropy_of_attribute)
 
 
@@ -31,19 +53,27 @@ def get_entropy_of_attribute(df, attribute):
 
 
 def get_information_gain(df, attribute):
-    information_gain = 0
+    information_gain = get_entropy_of_dataset(
+        df) - get_entropy_of_attribute(df, attribute)
     return information_gain
 
 
 ''' Returns Attribute with highest info gain'''
-#input: pandas_dataframe
-#output: ({dict},'str')
+# input: pandas_dataframe
+# output: ({dict},'str')
 
 
 def get_selected_attribute(df):
 
     information_gains = {}
-    selected_column = ''
+    selected_column = df.columns[0]
+    largest_ig = 0
+
+    for i in df.iloc[:, 0:len(df.columns)-1].columns:
+        information_gains[i] = get_information_gain(df, i)
+        if(largest_ig < information_gains[i]):
+            largest_ig = information_gains[i]
+            selected_column = i
 
     '''
 	Return a tuple with the first element as a dictionary which has IG of all columns 
